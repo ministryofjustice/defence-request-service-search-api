e5f041f014d643f72ab06eb34279d1d3907fd4f8
ansonK <ansonkelly@gmail.com>
2015-01-28 15:22:14 +0000

    return cors headers in responses for OPTIONS and HEAD requests

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module SolicitorSearchApi
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get]
+        resource '*', headers: :any, methods: [:get, :options, :head]
       end
     end
 
